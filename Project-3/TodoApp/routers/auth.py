b2f71a6d48f9e9926bd7760b60e9e33a8f114d72
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-24 21:56:18 +0530

    feat:  basic authentication api implemented

--- Project-3/TodoApp/routers/auth.py
+++ Project-3/TodoApp/routers/auth.py
@@ -1,13 +1,69 @@
-from fastapi import FastAPI, APIRouter
-
-# app = FastAPI()
-
-# @app.get("/auth/")
-# async def get_user():
-#     return {'user': 'authenticated'}
+from typing import Annotated
+from fastapi import FastAPI, APIRouter, Depends
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+    
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+# form_data: This parameter will be automatically populated with data from the request form. FastAPI uses OAuth2PasswordRequestForm to know what kind of data to expect.
+# OAuth2PasswordRequestForm: This is a class provided by FastAPI. It’s a special form class used to handle OAuth2 password grant flow. It expects data in a specific format, typically including fields like username and password.
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    
+    user = authenticate_user(form_data.username, form_data.password, db)
+    
+    if not user:
+        return "Failed Authentication"
+    
+    print("User's username: ", form_data.username, "  ", "User's password: ", form_data.password)
+    return "Successful Authentication"
+    
