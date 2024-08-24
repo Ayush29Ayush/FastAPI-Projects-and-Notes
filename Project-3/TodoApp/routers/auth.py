f40f4ff119da7f62ae973b4b7d74f3e4f95cd7c1
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-24 22:49:08 +0530

    refactor: Minor authentication enhancements

--- Project-3/TodoApp/routers/auth.py
+++ Project-3/TodoApp/routers/auth.py
@@ -10,13 +10,16 @@ from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import JWTError, jwt
 
-router = APIRouter()
+router = APIRouter(
+    prefix='/auth',
+    tags=['auth']
+)
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -72,7 +75,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user.')
 
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency,create_user_request: CreateUserRequest):
     create_user_model = Users(
         email=create_user_request.email,
@@ -97,7 +100,7 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     
     if not user:
         print("Failed Authentication")
-        return "Failed Authentication"
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='Could not validate user.')
     
     token  = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
     
