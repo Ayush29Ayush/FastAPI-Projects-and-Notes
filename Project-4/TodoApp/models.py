a0dc3a675030b19fcb9c62c915add28c88bc8e44
ayush senapati <ayushsenapati123@gmail.com>
2025-11-24 22:49:00 +0530

    feat: Project Setup with Pytest

---
+++ Project-4/TodoApp/models.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from pydantic import BaseModel
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+    phone_number = Column(String)
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    
+# ---------------------------------------------------------------------------------
+
+#! Output Model Schema
+
+class UserProfile(BaseModel):
+    id: int
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    phone_number: Optional[str] = None
+    role: str
+    is_active: bool
+
+    class Config:
+        from_attributes = True
