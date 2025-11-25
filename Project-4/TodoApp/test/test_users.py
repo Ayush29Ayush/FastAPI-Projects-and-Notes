cfe562737c84b00caf440183502e650beee97906
ayush senapati <ayushsenapati123@gmail.com>
2025-11-25 20:54:38 +0530

    feat: Added Test Files for Admin, Auth and Users

---
+++ Project-4/TodoApp/test/test_users.py
@@ -0,0 +1,41 @@
+from .utils import *
+from routers.users import get_db, get_current_user
+from fastapi import status
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+
+def test_return_user(test_user):
+    response = client.get("/user")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["username"] == "ayush"
+    assert response.json()["email"] == "ayush@email.com"
+    assert response.json()["first_name"] == "Ayush"
+    assert response.json()["last_name"] == "Senapati"
+    assert response.json()["role"] == "admin"
+    assert response.json()["phone_number"] == "1234567890"
+
+
+def test_change_password_success(test_user):
+    response = client.put(
+        "/user/password",
+        json={"password": "testpassword", "new_password": "newpassword"},
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_current_password(test_user):
+    response = client.put(
+        "/user/password",
+        json={"password": "wrong_password", "new_password": "newpassword"},
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == {
+        "detail": "Error on password change. Your password is incorrect."
+    }
+
+
+def test_change_phone_number_success(test_user):
+    response = client.put("/user/phonenumber/2222222222")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
