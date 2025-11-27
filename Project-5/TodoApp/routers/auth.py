21804b5d61c2d90f3637591166d7370fc7c2683b
ayush senapati <ayushsenapati123@gmail.com>
2025-11-27 10:35:15 +0530

    feat: Added routes

--- Project-5/TodoApp/routers/auth.py
+++ Project-5/TodoApp/routers/auth.py
@@ -1,6 +1,7 @@
 from datetime import timedelta, timezone, datetime
 from typing import Annotated
-from fastapi import FastAPI, APIRouter, Depends, HTTPException
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, Request
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from models import Users
 from passlib.context import CryptContext
@@ -45,6 +46,20 @@ def get_db():
         
 db_dependency = Annotated[Session, Depends(get_db)]
 
+templates = Jinja2Templates(directory="templates")
+
+
+### Pages ###
+
+@router.get("/login-page")
+def render_login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@router.get("/register-page")
+def render_register_page(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+### Endpoints ###
 def authenticate_user(username: str, password: str, db):
     user = db.query(Users).filter(Users.username == username).first()
     if not user:
